7a6452c6581529a7d9824fee26bbe4002b20d720
Francoise Umubyeyi <francoiseumubyeyi@Francoises-MacBook-Pro.local>
2022-01-16 18:22:10 -0500

    first commit

---
+++ ShopifyChallenge/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace ShopifyChallenge.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
